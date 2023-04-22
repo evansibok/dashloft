94b1013f8ba2f01054bb8bebb1dcd4d5ab3f93f4
Evans Ibok <evansibok@gmail.com>
2023-04-22 12:09:18 +0100

    add TRL type to reducer

--- src/state/types.ts
+++ src/state/types.ts
@@ -2,8 +2,15 @@ export interface ProductState {
   loading: boolean;
   error: string | null;
   product: Product | null;
+  trl: TRL | null;
 }
 
 export interface Product {
   [key: string]: any;
+}
+
+export interface TRL {
+  id: string;
+  name: string;
+  description?: string;
 }
